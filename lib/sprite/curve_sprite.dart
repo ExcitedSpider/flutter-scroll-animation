9e9887ae9c4630f79dd924dacd1b44cf584303c5
qefengg <qefeng@tencent.com>
2021-11-11 19:25:08 +0800

    chore: 补齐注释

--- lib/sprite/curve_sprite.dart
+++ lib/sprite/curve_sprite.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import "package:bezier/bezier.dart";
 import './base_sprite.dart';
 
+/// 实现基于贝塞尔曲线的曲线运动
 class CurveSprite extends BaseSprite {
-  /// 贝塞尔曲线
   /// https://pub.dev/packages/bezier
   final Bezier curve;
 
